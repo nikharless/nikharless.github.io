2eb818deb374b9c175ba70aaf745b5c4ba2d2097
nikharless <nik@ultratesting.us>
2025-04-08 10:51:20 -0500

    css remove

---
+++ a11y/assets/js/css-remove.js
@@ -0,0 +1,13 @@
+const cssStyles = true;
+
+function removeCSS() {
+	
+	if cssStyles == true {
+		document.getElementsByClassName("css-pos").removeAttribute("style");
+		const button = document.querySelector('cssButton');
+		button.innerHTML = "Reset";
+		cssStyles = false;
+	} else {
+		location.reload();
+	}
+}
