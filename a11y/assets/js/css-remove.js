0dd6ff6e18c44f5f4407ea8325a690275cc958fe
nikharless <nik@ultratesting.us>
2025-04-08 11:35:41 -0500

    css

--- a11y/assets/js/css-remove.js
+++ a11y/assets/js/css-remove.js
@@ -7,6 +7,10 @@ function removeCSS() {
 		for (var i=0; i < items.length; i++) {
 			items[i].removeAttribute("style");
 		}
+		const container = document.getElementById('status');
+		container.innerHTML = "The CSS positioning has been removed, and the lists are jumbled and make no sense.";
+		container.style.display = "block";
+
 		const button = document.getElementById('cssButton');
 		button.innerHTML = "Reset";
 		cssStyles = false;
