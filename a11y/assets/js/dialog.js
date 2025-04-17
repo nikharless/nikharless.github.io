baafc828c55691eb8ee916a49c4898b1942e43bc
nikharless <nik@ultratesting.us>
2025-04-16 19:03:30 -0500

    Update dialog.js

--- a11y/assets/js/dialog.js
+++ a11y/assets/js/dialog.js
@@ -119,7 +119,7 @@ function getObjectId(object) {
 
   aria.handleEscape = function (event) {
     var currentDialog = aria.getCurrentDialog();
-    alert(Object.keys(currentDialog.dialogNode.getAttribute('id')));
+    alert(currentDialog.dialogNode.getAttribute('id'));
     var failModal = document.getElementById('failModal');
     var key = event.which || event.keyCode;
     if (failModal && getObjectId(currentDialog) != 2) {
