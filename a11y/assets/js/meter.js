85585fdcc76748bcfeb40c390d099f7cb61b580c
nikharless <nik@ultratesting.us>
2025-06-07 01:04:22 -0500

    Update meter.js

--- a11y/assets/js/meter.js
+++ a11y/assets/js/meter.js
@@ -92,7 +92,6 @@ window.addEventListener('load', function () {
     return window.setInterval(function () {
       if (i !== undefined) {
         meters.forEach(function (meter) {
-          alert(meter.id);
           if (meters.findIndex(meter.id === i)) {
             meter.setValue(Math.random() * 100);
           }
