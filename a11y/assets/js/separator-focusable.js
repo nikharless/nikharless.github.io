7090ea59b5d033583e622d059cd7f8317db9b8c4
nikharless <nik@ultratesting.us>
2025-06-08 17:58:01 -0500

    Update separator-focusable.js

--- a11y/assets/js/separator-focusable.js
+++ a11y/assets/js/separator-focusable.js
@@ -3,6 +3,7 @@ function initSep () {
   const dragSep = document.getElementById("separator");
 
   dragSep.addEventListener('mousedown', mD);
+  dragSep.addEventListener('keydown', kD);
 }
 
 function mD(event) {
@@ -18,6 +19,36 @@ function mV(event) {
     end();
   }
 }
+
+function kD(event) {
+  switch (event.key) {
+    case "ArrowLeft":
+      pane1.style.flexBasis = pane1.style.flexBasis - 10%;
+      break;
+    case "ArrowRight":
+      pane1.style.flexBasis = pane1.style.flexBasis + 10%;
+      break;
+    case "Enter":
+      if (pane1.style.flexBasis > 0%) {
+        pane1.style.flexBasis = 0%;
+      } else {
+        pane1.style.flexBasis = 50%;
+      }
+      break;
+    case: "Home":
+      pane1.style.flexBasis = 0%;
+      break;
+    case: "End":
+      pane1.style.flexBasis = 100%;
+      break;
+    case: "F6":
+      break;
+    default:
+      return;
+  }
+  event.preventDefault();
+}
+
 const end = (e) => {
   ismdwn = 0;
   document.body.removeEventListener('mouseup', end);
