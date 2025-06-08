79efc09074cfbc4d8114406649fccc016b2a1300
nikharless <nik@ultratesting.us>
2025-06-08 02:30:32 -0500

    412 separator

---
+++ a11y/assets/js/separator-focusable.js
@@ -0,0 +1,21 @@
+let ismdwn = 0
+rpanrResize.addEventListener('mousedown', mD)
+
+function mD(event) {
+  ismdwn = 1
+  document.body.addEventListener('mousemove', mV)
+  document.body.addEventListener('mouseup', end)
+}
+
+function mV(event) {
+  if (ismdwn === 1) {
+    pan1.style.flexBasis = event.clientX + "px"
+  } else {
+    end()
+  }
+}
+const end = (e) => {
+  ismdwn = 0
+  document.body.removeEventListener('mouseup', end)
+  rpanrResize.removeEventListener('mousemove', mV)
+}
