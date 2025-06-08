28d3eda0f67b51b1c94812133d63bb0513d85d89
nikharless <nik@ultratesting.us>
2025-06-08 18:28:42 -0500

    Update separator-focusable.js

--- a11y/assets/js/separator-focusable.js
+++ a11y/assets/js/separator-focusable.js
@@ -24,23 +24,23 @@ function kD(event) {
   switch (event.key) {
     case "ArrowLeft":
       console.log(pane1.style.flexBasis);
-      pane1.style.flexBasis = pane1.style.flexBasis - 10%;
+      //pane1.style.flexBasis = pane1.style.flexBasis - 10%;
       break;
     case "ArrowRight":
-      pane1.style.flexBasis = pane1.style.flexBasis + 10%;
+      //pane1.style.flexBasis = pane1.style.flexBasis + 10%;
       break;
     case "Enter":
       if (pane1.style.flexBasis > 0%) {
-        pane1.style.flexBasis = 0%;
+        //pane1.style.flexBasis = 0%;
       } else {
-        pane1.style.flexBasis = 50%;
+        //pane1.style.flexBasis = 50%;
       }
       break;
     case: "Home":
-      pane1.style.flexBasis = 0%;
+      //pane1.style.flexBasis = 0%;
       break;
     case: "End":
-      pane1.style.flexBasis = 100%;
+      //pane1.style.flexBasis = 100%;
       break;
     case: "F6":
       break;
