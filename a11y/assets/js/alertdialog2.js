f63e170ee435d230b189158bd2ee40e3d43f3df3
nikharless <nik@ultratesting.us>
2025-06-21 04:30:50 -0500

    412

--- a11y/assets/js/alertdialog2.js
+++ a11y/assets/js/alertdialog2.js
@@ -1,30 +1,52 @@
-const modal = document.querySelector(".modal");
+'use strict';
+
 const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
-const submitBtn = document.querySelector(".btn-submit");
-
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-  overlay.style.display="block";
-};
-
-openModalBtn.addEventListener("click", openModal);
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-  overlay.style.display="none";
-  openModalBtn.focus();
-};
-
-closeModalBtn.addEventListener("click", closeModal);
-submitBtn.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    modalClose();
-  }
-});
+class AlertModal {
+    constructor(alertNode) {
+        this.alertNode = alertNode;
+
+        var modal = this.alertNode.querySelector(".modal");
+        var openModalBtn = this.alertNode.querySelector(".btn-open");
+        var closeModalBtn = this.alertNode.querySelector(".btn-close");
+        var submitBtn = this.alertNode.querySelector("btn-submit");
+
+        openModalBtn.addEventListener('click', this.openModal.bind(this.openModalBtn));
+        closeModalBtn.addEventListener('click', this.closeModal.bind(this.closeModalBtn));
+        submitBtn.addEventListener('click', this.closeModal.bind(this.submitBtn));
+    }
+
+    openModal = function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+        overlay.style.display="block";
+    };
+
+    closeModal = function () {
+        modal.classList.add("hidden");
+        overlay.classList.add("hidden");
+        overlay.style.display="none";
+        openModalBtn.focus();
+    };
+
+    document.addEventListener("keydown", function (e) {
+        if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+            closeModal();
+        }
+    });
+}
+
+window.addEventListener(
+  'load',
+  function () {
+    var alerts = document.querySelectorAll(
+      '.alertdialog'
+    );
+
+    for (var i = 0; i < alerts.length; i++) {
+      new AlertModal(alerts[i]);
+    }
+  },
+  false
+);
