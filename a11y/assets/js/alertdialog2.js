04ea9b2d4d9baf6585647daad0900e04e0a42531
nikharless <nik@ultratesting.us>
2025-06-21 03:31:52 -0500

    412

---
+++ a11y/assets/js/alertdialog2.js
@@ -0,0 +1,30 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const submitBtn = document.querySelector(".btn-submit");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  overlay.style.display="block";
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  overlay.style.display="none";
+  openModalBtn.focus();
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+submitBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    modalClose();
+  }
+});
